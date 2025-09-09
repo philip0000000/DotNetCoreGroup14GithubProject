69ce4a7424fad86b81b93a1c353e33535b1a6d91
fsGe77hU3giMfse <92891603+philip0000000@users.noreply.github.com>
2025-09-09 13:19:19 +0200

    Add project files.

---
+++ DotNetCoreGroup14GithubProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCoreGroup14GithubProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
