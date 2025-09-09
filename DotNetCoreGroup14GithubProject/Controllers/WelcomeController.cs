aa4ba8282c36a39b28537d4260d21225cd6ac952
ezgikara58 <ezgikara58@gmail.com>
2025-09-09 13:53:32 +0200

    Added new WelcomeController

---
+++ DotNetCoreGroup14GithubProject/Controllers/WelcomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreGroup14GithubProject.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
